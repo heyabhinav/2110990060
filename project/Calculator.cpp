f1e997421808fcbe0933db84dd272a60fd34c3ed
heyabhinav <abhinav0050.be21@chitkara.edu.in>
2022-05-24 18:46:34 +0530

    Added subtraction functionality and default case, also formatted code little bit.

--- project/Calculator.cpp
+++ project/Calculator.cpp
@@ -2,20 +2,23 @@
 using namespace std;
 
 int main(){
-    int a,b,r;
+    int a, b, r;
     char ch;
 
-    cout<<"Enter operands (a and b): ";
-    cin>>a>>b;
+    cout << "Enter operands (a and b): ";
+    cin >> a >> b;
 
-    cout<<"Enter the operator :";
-    cin>>ch;
+    cout << "Enter the operator :";
+    cin >> ch;
 
     switch(ch)
 {
     case '+' : r = a + b;
                break;
+    case '-' : r = a - b;
+               break;
+    default: cout << "Invalid Operator!";
 }
-cout<<"Result: "<<r;
-return 0;
+    cout<<"Result: "<<r;
+    return 0;
 } 
